fbedc2b10c2813e7fb01513a484242390fdd3cde
ismail00b <ismail00b@gmail.com>
2014-09-29 13:04:23 +0200

    Update Untitled1.js

--- testConflict/Untitled1.js
+++ testConflict/Untitled1.js
@@ -2,5 +2,5 @@
 <<<<<<< HEAD
 Okiii test (edited locally)!
 =======
-Okiii test (edited from github) !
+Okiii test (edited from github - from github) !
 >>>>>>> c0c8e849f8cd5f20bef1e314377229194efb9637
